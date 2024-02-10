e8fb9730cd7acc73c920645d38af0c4d0ae892d1
Jerin Jose <jerin.j.johnson@opscruise.com>
2024-02-10 20:43:49 +0530

    updated the status

--- src/app/components/leadManagement/deals/kanban-list/kanban-list.component.ts
+++ src/app/components/leadManagement/deals/kanban-list/kanban-list.component.ts
@@ -92,7 +92,7 @@ export class KanbanListComponent implements OnInit {
         if (event.previousContainer === event.container) {
             moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
         } else {
-            if(!this.keycloakService.isUserInRole('manage-quote') && event.container.id ==='5') { return }
+            if(!this.keycloakService.isUserInRole('manage-quote') && event.container.id ==='4') { return }
             const card = event.previousContainer.data[event.previousIndex];
             card.status = _.find(dealStatus, (s) => s.listId === event.container.id)?.name;
             this.xService.updateX('deal', card, card.id);
